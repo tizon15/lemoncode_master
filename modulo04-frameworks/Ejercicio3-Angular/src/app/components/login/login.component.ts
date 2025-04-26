b0588e7092b8fed07274ef4dc5edc397b0687ec6
Álvaro Martínez Tizón García <amartineztizon@gmail.com>
2025-04-26 18:58:32 +0200

    adding the login page

--- modulo04-frameworks/Ejercicio3-Angular/src/app/components/login/login.component.ts
+++ modulo04-frameworks/Ejercicio3-Angular/src/app/components/login/login.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { LoginService } from '../../services/login.service';
 @Component({
-  selector: 'app-login',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  private loginService = inject(LoginService);
+  private fb = inject(FormBuilder);
+  form = this.fb.group({
+    username: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  });
+  hide = signal(true);
+  get username() {
+    return this.form.controls['username'];
+  }
+  get password() {
+    return this.form.controls['password'];
+  }
+  errorMessage = signal('');
 
+  constructor() {}
+
+  updateErrorMessage() {
+    if (this.username.hasError('required') || this.password.hasError('required')) {
+      this.errorMessage.set('You must enter a value');
+    } else if (this.username.hasError('email')) {
+      this.errorMessage.set('Not a valid email');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
+
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+  handleLogin() {
+    const { username, password } = this.form.value;
+    this.loginService.login(username!, password!);
+  }
 }
--- modulo04-frameworks/Ejercicio3-Angular/src/app/services/login.service.ts
+++ modulo04-frameworks/Ejercicio3-Angular/src/app/services/login.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService {
+  private username: string = 'master@lemoncode.net';
+  private password: string = '12345678';
+  constructor() {}
 
-  constructor() { }
+  login(username: string, password: string): boolean {
+    if (username === this.username && password === this.password) {
+      return true;
+    }
+    return false;
+  }
+  logout() {
+    return true;
+  }
+  isLogged() {
+    return true;
+  }
+  getUserName() {
+    return 'XXXXXXXXXXXXXXX';
+  }
 }
