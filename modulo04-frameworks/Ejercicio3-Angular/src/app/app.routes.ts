a50522d7c75fe4664bc60d496b8b5e4404c4b104
Álvaro Martínez Tizón García <amartineztizon@gmail.com>
2025-04-26 18:11:08 +0200

    navbar and components & services

--- modulo04-frameworks/Ejercicio3-Angular/src/app/app.routes.ts
+++ modulo04-frameworks/Ejercicio3-Angular/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
